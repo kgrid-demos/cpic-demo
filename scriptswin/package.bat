591a69c8b676f98b1d20637cd705f6489fe42ee5
farrisg <farrisg@umich.edu>
2018-09-12 16:39:01 -0400

    Change windows build to package...

---
+++ scriptswin/package.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rmdir /s /q dist
+mkdir dist\cpic-kit
+dir
+xcopy  package.json dist\cpic-kit
+xcopy  package-lock.json dist\cpic-kit
+xcopy  scripts dist\cpic-kit\scripts\
+xcopy  scripts dist\cpic-kit\scriptwin\
+xcopy  cli-client dist\cpic-kit\cli-client\
+xcopy  web dist\cpic-kit\web\
+
+cd dist
+zip -r cpic-kit.zip cpic-kit
+
+rmdir /s /q cpic-kit
