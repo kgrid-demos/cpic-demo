579ba4c2f1114e674bc1324c833fdd708153b403
Greg Farris <greg.t.farris@gmail.com>
2018-10-01 11:11:47 -0400

    localhost

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,13 +8,12 @@ nohup java -jar library/kgrid-library.jar --server.port=8081 --kgrid.shelf.cdost
 echo "Starting activator on http://localhost:8082"
 nohup java -jar activator/kgrid-activator.jar --server.port=8082 --kgrid.shelf.cdostore.url=filesystem:file://activator/shelf &>/dev/null &
 
-echo "Waiting....."
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8081)" != "200" ]]; do sleep 5; done
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8082)" != "200" ]]; do sleep 5; done
-
 echo "Starting CPIC Kit demo web site on http://localhost:8083"
 cd web &&  nohup ../node_modules/http-server/bin/http-server -p 8083 -a localhost &
 
+echo "Waiting....."
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8081)" != "200" ]]; do sleep 5; done
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8082)" != "200" ]]; do sleep 5; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8083)" != "200" ]]; do sleep 5; done
 
 echo "KGrid CPIC Kit is running"
