029afe27d0cf1af5514671a2c81c7b4839b40089
circleci <kgrid-developers@umich.edu>
2018-10-04 15:47:34 -0400

    Once more, trying to ingore gh-pages

--- .circleci/vuepress_deploy.sh
+++ .circleci/vuepress_deploy.sh
@@ -9,13 +9,15 @@ git config --global user.name "circleci"
 npm run docs:build
 # copy web demo to github pages dist
 cp -r web docs/.vuepress/dist
+mkdir -p docs/.vuepress/dist/.circleci
+cp -a .circleci/. docs/.vuepress/dist/.circleci/.
 
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://${GITHUB_TOKEN}@github.com/kgrid-demos/cpic-kit.git master:gh-pages
