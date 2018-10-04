6f8fb68e7a6415bde743da55afc9cb9a8e6660f7
Greg Farris <greg.t.farris@gmail.com>
2018-10-04 13:39:39 -0400

    Adding Vue Press deploy script for circle

---
+++ .circleci/vuepress_deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+# copy web demo to github pages dist
+cp web docs/.vuepress/dist
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:kgrid-demos/cpic-kit.git master:gh-pages
